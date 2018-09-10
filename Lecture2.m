084769a638de32b76679ea42ea4e77e9dceec3f7
Ecyg <cyg1093065@gmail.com>
2018-09-10 14:07:38 -0500

    Update Lecture2.m

--- Lecture2.m
+++ Lecture2.m
@@ -91,7 +91,7 @@ hist(x) % Histogram of the x vector
 F = randn(3,3) %randn is a normal distribution set of random numbers
 
 figure(2)
-x = randn(1,900000); % 900k set of random numbers
+x = randn(1,900000); % 900k set of random numbers. This is a Uniform Distribution
 hist(x)
 
 %--------Make it cleaner
@@ -104,4 +104,70 @@ figure(2)
 x = randn(1,900000); % 900k set of random numbers
 hist(x,500) % See? This one looks better
 
+%Improved Version
+clear X
+Ntrial = 900000; % Number of trials
+Nbin = 150; % This is the number of bars along the graph
+X = randn(1,Ntrial);
+hist(X, Nbin)
 
+
+%--------------------------------------
+% Random walk with discrete time from N(mu, sigma)
+%--------------------------------------
+clear all
+mu = 0;
+sigma = 1;
+x = [0];
+Nmax = 100;
+
+% What do this mean? Ok, so where you're going x(i+1) is 
+%determined by where I am x(i) + normally distrubted random number * the 
+%standard deviation
+for i = [1:Nmax-1]
+    x(i+1) = x(i)+(mu+randn*sigma);
+end
+plot(1:Nmax, x)
+grid on
+
+
+%---------------------------------------------
+%Random walk for 10 different paths
+%---------------------------------------------
+
+clear all
+mu = 0;
+sigma = 1;
+x = 0;
+Nmax = 100;
+all = 10; %This is for HOW MANY people are doing it
+
+for path = 1:all
+    for i = [1:Nmax-1]
+     x(i+1) = x(i)+(mu+randn*sigma);
+    end
+    plot(1:Nmax,x)
+    hold on %Means don't plot just yet, not done
+end
+grid on
+hold off
+
+%-----------------------------------------
+% Random Walk with "continuous" time from N(mu, sigma)
+% Brownian Motion : A Random Walk continuous in time
+%---------------------------------------------------
+clear all
+mu = 0;
+sigma = 1;
+dt = 0.01;
+t = 0 : dt : 10;
+x = 0;
+Nmax = length(t);
+
+for i = 1:Nmax-1
+    x(i+1) = x(i)+(sqrt(dt)*mu+sigma*randn);
+end
+figure(6)
+plot(t,x)
+title('Brownian Motion');
+grid on
