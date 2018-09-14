06a8424c3f2c5aff666c53f2f6c343f3149e84b8
Ecyg <cyg1093065@gmail.com>
2018-09-14 10:14:49 -0500

    Update Hmwk3.m

--- Hmwk3.m
+++ Hmwk3.m
@@ -1,148 +1,47 @@
+
 % Hmwk 3 
-
-
- 
-
-
- 
-
-
 clear all
 
-
 nmax = 200;
-
-
 mu1 = 0.8;
-
-
 mu2 = 1.004;
-
-
 mu3 = 1.03;
 
-
-x = zeros(1, nmax);
-
-
-y = zeros(1, nmax);
-
-
-z = zeros(1, nmax);
-
-
 x(1) = rand;
+y(1) = rand;
+z(1) = rand;
 
-
- 
-
-
-for i = [1 : nmax-1]
-
-
-    if (x(i) <= 0.5 && x(i) >= 0)
-
-
-        x(i+1) = mu1*x(i);
-
-
-        
-
-
-    elseif (x(i) > 0.5 && x(i) <= 1)
-
-
-        x(i+1) = mu1*(1-x(i));
-
-
-        
-
-
+if (x(1) <= 0.5 )
+   for i = [1 : nmax - 1] 
+      x(i+1) = mu1*x(i); 
+   end
+else
+    for a = [1 : nmax - 1]
+       x(a+1) = mu1*(1-x(a)); 
     end
-
-
 end
-
-
-x';
-
-
-grid on
-
-
- 
-
-
- 
-
+plot(1:nmax,x)
 
 % Mu 2
-
-
-for i = [1 : nmax-1]
-
-
-    if (x(i) <= 0.5 && x(i) >= 0)
-
-
-        x(i+1) = mu2*x(i);
-
-
-        
-
-
-    elseif (x(i) > 0.5 && x(i) <= 1)
-
-
-        x(i+1) = mu2*(1-x(i));
-
-
-        
-
-
+if (y(1) <= 0.5 )
+   for j = [1 : nmax - 1] 
+      y(j+1) = mu2*y(j); 
+   end
+else
+    for b = [1 : nmax - 1]
+       y(b+1) = mu2*(1-y(b)); 
     end
-
-
 end
-
-
- 
-
-
- 
-
-
- 
-
-
- 
-
+plot(1:nmax,y)
 
 % Mu 3
-
-
-for i = [1 : nmax-1]
-
-
-    if (x(i) <= 0.5 && x(i) >= 0)
-
-
-        x(i+1) = mu3*x(i);
-
-
-        
-
-
-    elseif (x(i) > 0.5 && x(i) <= 1)
-
-
-        x(i+1) = mu3*(1-x(i));
-
-
-        
-
-
+if (z(1) <= 0.5 )
+   for k = [1 : nmax - 1] 
+      z(k+1) = mu3*z(k); 
+   end
+else
+    for c = [1 : nmax - 1]
+       z(c+1) = mu3*(1-z(c)); 
     end
-
-
 end
+plot(1:nmax,z)
